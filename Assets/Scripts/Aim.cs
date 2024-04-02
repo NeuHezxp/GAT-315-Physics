94bdf124872dd2d402ad090e96002b45a4f87508
NeuHezxp <mpatino@student.neumont.edu>
2024-04-02 09:59:04 -0600

    added various assignments

--- Assets/Scripts/Aim.cs
+++ Assets/Scripts/Aim.cs
@@ -1,40 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.Rendering;
 using UnityEngine;
 
 public class Aim : MonoBehaviour
 {
-    public float sensitivity = 3;
+    [SerializeField] float speed = 3;
+
     Vector3 rotation = Vector3.zero;
-    Vector2 prevAxis = Vector2.zero;
-    // Start is called before the first frame update
+
     void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
-
-        prevAxis.y = Input.GetAxis("Mouse X");
-        prevAxis.x = -Input.GetAxis("Mouse Y");
     }
 
-    // Update is called once per frame
     void Update()
     {
         Vector3 axis = Vector3.zero;
-        axis.y = Input.GetAxis("Mouse X") - prevAxis.x;
-        axis.x = -Input.GetAxis("Mouse Y") - prevAxis.y;
+        axis.x = -Input.GetAxis("Mouse Y");
+        axis.y = Input.GetAxis("Mouse X");
 
+        rotation.x += axis.x * speed;
+        rotation.y += axis.y * speed;
 
-        rotation.x += axis.x * sensitivity;
-        rotation.y += axis.y * sensitivity;
+        rotation.x = Mathf.Clamp(rotation.x, -50, 50);
+        rotation.y = Mathf.Clamp(rotation.y, -70, 70);
 
-        rotation.x = Mathf.Clamp(rotation.x, 40, 40);
-        rotation.y = Mathf.Clamp(rotation.y, -40, 40);
+        Quaternion qyaw = Quaternion.AngleAxis(rotation.y, Vector3.up);
+        Quaternion qpitch = Quaternion.AngleAxis(rotation.x, Vector3.right);
 
-        Quaternion qYaw = Quaternion.AngleAxis(axis.y * sensitivity, Vector3.up);
-        Quaternion qPitch = Quaternion.AngleAxis(axis.x * sensitivity, Vector3.right);
-
-        transform.localRotation = (qYaw *qPitch );
+        transform.localRotation = qyaw * qpitch;
     }
 }
