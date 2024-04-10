378baddb10cb5af165f7983de8c1e450ad63acb7
NeuHezxp <mpatino@student.neumont.edu>
2024-04-10 10:14:52 -0600

    made small changes to character. DIDNT WORK

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -14,11 +14,12 @@ public class CharacterMovement : MonoBehaviour
     private CharacterController controller;
     private Vector3 velocity;
     private bool onGround;
+    public Animator animator; // Reference to the Animator component
 
     private void Start()
     {
         controller = gameObject.GetComponent<CharacterController>();
-        
+        animator = gameObject.GetComponent<Animator>(); // Initialize the Animator component
     }
 
     void Update()
@@ -30,7 +31,7 @@ public class CharacterMovement : MonoBehaviour
         }
 
         Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-        move = Vector3.ClampMagnitude(move, 1); // makes diagonals the same sped as directionals
+        move = Vector3.ClampMagnitude(move, 1); // makes diagonals the same speed as directionals
 
         // puts us in view space
         move = Quaternion.Euler(0, cameraView.rotation.eulerAngles.y, 0) * move;
@@ -39,11 +40,17 @@ public class CharacterMovement : MonoBehaviour
 
         if (move != Vector3.zero)
         {
-            //gameObject.transform.forward = move;
-            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(move), Time.deltaTime * rotationRate); // lookRotation makes a rotation out of a vector
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(move), Time.deltaTime * rotationRate);
         }
 
-        // Changes the height position of the player..
+        // Updating Animator parameters
+        animator.SetFloat("Speed", velocity.magnitude);
+        animator.SetFloat("YVal", velocity.y);
+        animator.SetBool("OnGround", onGround);
+       
+        // animator.SetBool("Equipped", isEquipped);
+
+        // Jumping
         if (Input.GetButtonDown("Jump") && onGround)
         {
             velocity.y += Mathf.Sqrt(jumpHeight * -3.0f * Physics.gravity.y);
@@ -52,31 +59,22 @@ public class CharacterMovement : MonoBehaviour
         velocity.y += Physics.gravity.y * Time.deltaTime;
         controller.Move(velocity * Time.deltaTime);
     }
+
     void OnControllerColliderHit(ControllerColliderHit hit)
     {
         Rigidbody body = hit.collider.attachedRigidbody;
 
-        // no rigidbody
         if (body == null || body.isKinematic)
         {
             return;
         }
 
-        // We dont want to push objects below us
         if (hit.moveDirection.y < -0.3)
         {
             return;
         }
 
-        // Calculate push direction from move direction,
-        // we only push objects to the sides never up and down
         Vector3 pushDir = new Vector3(hit.moveDirection.x, 0, hit.moveDirection.z);
-
-        // If you know how fast your character is trying to move,
-        // then you can also multiply the push velocity by that.
-
-        // Apply the push
         body.AddForce(pushDir * pushPower, ForceMode.VelocityChange);
-        //body.velocity = pushDir * pushPower;
     }
 }
